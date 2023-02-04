85743bbda585a53888fa2bf5a93451614038d815
Innomer <innomershyper@gmail.com>
2023-02-04 17:35:19 +0530

    Product Made

---
+++ odin-shopping-cart/src/Product.js
@@ -0,0 +1,8 @@
+export default function Product(props){
+    return(
+        <div>
+            <p>{props.name}</p>
+            <p>{props.price}</p>
+        </div>
+    );
+}
--- odin-shopping-cart/src/ProductPage.js
+++ odin-shopping-cart/src/ProductPage.js
@@ -1,12 +1,20 @@
 import BottomBar from "./BottomBar";
 import NavBar from "./NavBar";
+import Product from "./Product";
 
 function ProductPage(){
+    var productList=[{name:"Backpack",price:19.99},{name:"Men's Casual TShirt",price:50.99},{name:"Men's Cotton Jacket",price:59.99},{name:"Women's Rain Jacket (Long)",price:39.99}];
     return(
         <div>
             <NavBar/>
             <div>
-                
+                <div>
+                    {productList.map(product=>{
+                        return(
+                            <Product name={product.name} price={product.price} />
+                        )
+                    })}
+                </div>
             </div>
             <BottomBar/>
         </div>
